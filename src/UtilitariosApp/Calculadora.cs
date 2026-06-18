1c19fddd1fcead65d7adbfff447419ff47399d5b
isadora <isadoraalmeidapaixao@gmail.com>
2026-06-18 20:14:26 -0300

    estrutura inicial do projeto

---
+++ src/UtilitariosApp/Calculadora.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UtilitariosApp
+{
+    public class Calculadora
+    {
+        public int Somar(int a, int b)
+        {
+            return a + b;
+        }
+        public int Multiplicar(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
---
+++ src/UtilitariosApp/Program.cs
@@ -0,0 +1,7 @@
+﻿using UtilitariosApp;
+
+Calculadora c1 = new Calculadora();
+
+var resultado = c1.Somar(14, 17);
+
+Console.WriteLine($"O resultado da soma é: {resultado}");
