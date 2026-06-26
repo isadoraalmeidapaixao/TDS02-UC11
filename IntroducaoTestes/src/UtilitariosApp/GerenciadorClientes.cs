7335a9233c64711ab35544428fd64241bf095ce5
isadora <isadoraalmeidapaixao@gmail.com>
2026-06-25 22:00:27 -0300

    exemplos de testes

--- IntroducaoTestes/src/UtilitariosApp/GerenciadorClientes.cs
+++ IntroducaoTestes/src/UtilitariosApp/GerenciadorClientes.cs
@@ -12,12 +12,44 @@ namespace UtilitariosApp
         private List<Cliente> clientes = new();
         public void AdicionarCliente(Cliente cliente)
         {
-            // Lógica para adicionar o cliente
+            if (cliente == null)
+            {
+                throw new ArgumentNullException(nameof(cliente));
+            }
+                clientes.Add(cliente);
         }
 
         public List<Cliente> ObterTodosClientes()
         {
             return clientes;
+        }
+
+        public int ContarClientes()
+        {
+            return clientes.Count;
+        }
+
+        public Cliente? ProcurarPorId(int id)
+        {
+            foreach (var cliente in clientes)
+            {
+                if (cliente.Id == id) return cliente;
+            }
+
+            return null;
+        }
+
+        public bool RemoverPorId(int id)
+        {
+            var cliente = ProcurarPorId(id);
+
+            if (cliente != null)
+            {
+                clientes.Remove(cliente);
+                return true;
+            }
+
+            return false;
         }
     }
 }
